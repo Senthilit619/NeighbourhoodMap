1edf870512b450129582daf4be7aac520cc25933
senthil <krsenthilit619@gmail.com>
2016-12-07 20:34:11 +0530

    Info Window Added

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
  var favlocations=[
-            {title: 'Isha Yoga', location:{lat: 10.9789623,lng: 76.7365913}}, 
-            {title: 'Ajantha Theatres',location: {lat: 10.9601563,lng: 78.0708733}}, 
-            {title: 'Erode Bus Stand',location: {lat: 11.3467973,lng: 77.7172613}}, 
-            {title: 'Hotel Hemala',location: {lat: 10.9610723,lng: 78.0804773}}, 
-            {title: 'Dindigal Thalapakkati',location: {lat: 10.9590474,lng: 78.0735335}}, 
-            {title: 'The Chennai Silks',location: {lat: 10.9624398,lng: 78.0679048}}, 
-            {title: 'Kolli Hills',location: {lat: 11.3392332,lng: 78.2381341}}, 
-            {title: 'Adyar Anandha Bhavan',location: {lat: 10.9613173,lng: 78.0666256}}, 
-            {title: 'NTS Palace',location: {lat: 10.9616167,lng: 78.0620733}}, 
-            {title: 'Joy Alukkas',location: {lat: 10.9616167,lng: 78.0620733}}
+            {title: 'Isha Yoga', location:{lat: 10.9789623,lng: 76.7365913},id:1}, 
+            {title: 'Ajantha Theatres',location: {lat: 10.9601563,lng: 78.0708733},id:2}, 
+            {title: 'Erode Bus Stand',location: {lat: 11.3467973,lng: 77.7172613},id:3}, 
+            {title: 'Hotel Hemala',location: {lat: 10.9610723,lng: 78.0804773},id:4}, 
+            {title: 'Dindigal Thalapakkati',location: {lat: 10.9590474,lng: 78.0735335},id:5}, 
+            {title: 'The Chennai Silks',location: {lat: 10.9624398,lng: 78.0679048},id:6}, 
+            {title: 'Kolli Hills',location: {lat: 11.3392332,lng: 78.2381341},id:7}, 
+            {title: 'Adyar Anandha Bhavan',location: {lat: 10.9613173,lng: 78.0666256},id:8}, 
+            {title: 'NTS Palace',location: {lat: 10.9616167,lng: 78.0620733},id:9}, 
+            {title: 'Joy Alukkas',location: {lat: 10.9616167,lng: 78.0620733},id:10}
   ];
 
 // Global variables
 var map;
 var markers=[];
+
 var vm;
+
 function initMap() {
        map = new google.maps.Map(document.getElementById('map'),{
         center:{lat:10.957025,lng:78.066409},
@@ -28,6 +30,7 @@ function initMap() {
   }
 var ViewModel=function(){
         "use strict";
+        var largeInfowindow = new google.maps.InfoWindow();
         var self = this;
         self.locations= ko.observableArray(favlocations);
         self.locations().forEach(function(loc){
@@ -38,7 +41,31 @@ var ViewModel=function(){
           });
           loc.marker=marker;
           markers.push(marker);
+          //Add an event listener to open the Infowindow
+          marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
         });
-      };
+      });
+      //Function to execute when the user clicks the listview 
+      self.selectloc = function(loc){
+        loc.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () {
+            loc.marker.setAnimation(null);
+        }, 1000);
+      }
+}
+      // Function to open the InfoWindow onclick
+      function populateInfoWindow(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+        }
+      }
 
 
