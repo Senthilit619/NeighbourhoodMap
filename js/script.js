e9a90ee154ca30e8f668d340026183ba7551d93e
senthil <krsenthilit619@gmail.com>
2016-12-09 16:19:02 +0530

    First submit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
+ //Model
  var favlocations=[
-            {title: 'Isha Yoga', location:{lat: 10.9789623,lng: 76.7365913},id:1}, 
-            {title: 'Ajantha Theatres',location: {lat: 10.9601563,lng: 78.0708733},id:2}, 
-            {title: 'Erode Bus Stand',location: {lat: 11.3467973,lng: 77.7172613},id:3}, 
-            {title: 'Hotel Hemala',location: {lat: 10.9610723,lng: 78.0804773},id:4}, 
-            {title: 'Dindigal Thalapakkati',location: {lat: 10.9590474,lng: 78.0735335},id:5}, 
-            {title: 'The Chennai Silks',location: {lat: 10.9624398,lng: 78.0679048},id:6}, 
-            {title: 'Kolli Hills',location: {lat: 11.3392332,lng: 78.2381341},id:7}, 
-            {title: 'Adyar Anandha Bhavan',location: {lat: 10.9613173,lng: 78.0666256},id:8}, 
-            {title: 'NTS Palace',location: {lat: 10.9616167,lng: 78.0620733},id:9}, 
-            {title: 'Joy Alukkas',location: {lat: 10.9616167,lng: 78.0620733},id:10}
-  ];
+            {title: 'Delhi sweats & bakery', location:{lat: 10.956583,lng:78.081597}}, 
+            {title: 'Hotel Hemala',location: {lat: 10.96106536392226 ,lng: 78.08258976687684}}, 
+            {title: 'Planet Fashion',location: {lat: 10.959212 ,lng:  78.079608}}, 
+            {title: 'Karur Bus Terminus',location: {lat: 10.9593501,lng:78.0741122}}, 
+            {title: 'Dindigal Thalapakatti',location: {lat: 10.9590758,lng:78.0756299}}, 
+            {title: 'Basics life',location: {lat: 10.959771,lng: 78.074313}}, 
+            {title: 'Adyar Anandha Bhavan',location: {lat: 10.9592703,lng:78.0761873}}, 
+            {title: 'NTS Palace',location: {lat: 10.961620,lng:78.064263}}, 
+            {title: 'Hotel Valluvar',location: {lat: 10.9606905,lng:78.0759959}}
+  ];                                                 
 
 // Global variables
 var map;
@@ -32,8 +32,9 @@ var ViewModel=function(){
         var bounds = new google.maps.LatLngBounds();
         var self = this;
         self.locations= ko.observableArray(favlocations);
+        //Input given by the user as search term.
         self.searchterm = ko.observable('');
-
+        // Marker for all the locations in the locations array.
         self.locations().forEach(function(loc){
             var marker = new google.maps.Marker({
             title:loc.title,
@@ -60,6 +61,7 @@ var ViewModel=function(){
         //Search Function
         self.searchfunction = ko.computed(function(){
          var search = self.searchterm();
+         //Iterate through all the location in the array and find if the search location is available and set the corresponding marker as visible. 
          for (var i = 0; i < self.locations().length; i++) {
             if (self.locations()[i].title.toLowerCase().indexOf(search)>=0) 
                 {
@@ -76,39 +78,44 @@ var ViewModel=function(){
     
     }
 
-        //Function to execute when the user clicks the listview 
-        self.selectloc = function(loc){
-            loc.marker.setAnimation(google.maps.Animation.BOUNCE);
-            setTimeout(function () {
-            loc.marker.setAnimation(null);
-            }, 1400);
-          }
+//Function to execute when the user clicks the listview 
+self.selectloc = function(loc){
+        loc.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function () {
+        loc.marker.setAnimation(null);
+        }, 1400);
+    }
 
 
 
-        // Function to open the InfoWindow onclick
-        function populateInfoWindow(marker, infowindow) {
-          
+// Function to open the InfoWindow onclick
+function populateInfoWindow(marker, infowindow) {
         if (infowindow.marker != marker) 
-          infowindow.marker = marker;
+          {
+            infowindow.marker = marker;
+            infowindow.setContent("Loading...");
+            infowindow.open(map,marker);
+          }
+        // Client ID and Secret for making the Fooursquare API request
         var client_id = 'XX02XSETTBB01M0KZUF5SLTEWX5NG0M4401FCHWJZLLYWM15',
             client_secret = 'VXPOQENZXZ2QI2WCKGFBIOSBWK134ARVM05E2PB15A5XKAZI',
             lat=marker.position.lat(),
             lng=marker.position.lng();
             var pos = lat +','+ lng;
+            //AJax request for the foursquare API to get the location data.
             var url = 'https://api.foursquare.com/v2/venues/search?ll=' + pos + '&client_id=' + client_id + '&client_secret=' + client_secret + '&v=20161129&m=foursquare';
                 $.ajax({
                         method: 'GET',
                         url: url,
                         dataType: 'jsonp',
                         success: function (data) {
+                                  //Store the data from the ajax request.
                                   var name=data.response.venues[0].name;
+                                  //venue id is for making the second ajax request to foursquare to get the photos of that place. 
                                   var venue_id = data.response.venues[0].id;
                                   var address = data.response.venues[0].location.address;
                                   var counts = data.response.venues[0].stats.checkinsCount;
-                                  
                                   //Second ajax request for fetching the photos.
-                                  //api.foursquare.com/v2/venues/43695300f964a5208c291fe3/photos?client_id=XX02XSETTBB01M0KZUF5SLTEWX5NG0M4401FCHWJZLLYWM15&client_secret=VXPOQENZXZ2QI2WCKGFBIOSBWK134ARVM05E2PB15A5XKAZI&v=20161208
                                   var secondurl = 'https://api.foursquare.com/v2/venues/' + venue_id +'/photos?client_id=' + client_id + '&client_secret=' + client_secret + '&v=20161129';
                                   $.ajax({
                                           method: 'GET',
@@ -120,36 +127,43 @@ var ViewModel=function(){
                                             var item = data.response.photos.items[0];
                                             if(item==undefined)
                                             { 
-                                              infowindow.setContent('<div><span id="name">' + name + '</span><br>Address:'+ address+'<br>Check-ins:'+ counts + '</div><div>No photos available for this location</div>');
+                                              //If there is no photo, then display the message.
+                                              infowindow.setContent('<div><span class="bold"> ' + name + '</span><br><span class="bold">Address: </span>'+ address+'<br><span class="bold">Check-ins: </span>'+ counts + '</div><div>No photos available for this location</div>');
                                               infowindow.open(map, marker);
-                                              // Make sure the marker property is cleared if the infowindow is closed.
                                               infowindow.addListener('closeclick', function() {
                                               infowindow.marker = null;
                                               });
                                             }
                                             else
                                             {
+                                              //If there are photos, display the first one.
                                               var prefix = data.response.photos.items[0].prefix;
                                               var suffix = data.response.photos.items[0].suffix;
                                               var src = prefix + '100x100' +suffix;
-                                              infowindow.setContent('<div><span id="name">' + name + '</span><br>Address:'+ address+'<br>Check-ins:'+ counts + '</div><div>Photo:<br><img src="'+src+'"></div>');
+                                              infowindow.setContent('<div><span class="bold"> ' + name + '</span><br><span class="bold">Address: </span>'+ address+'<br><span class="bold">Check-ins: </span>'+ counts + '</div><div>Photo :<br><img src="'+src+'"></div>');
                                               infowindow.open(map, marker);
-                                              // Make sure the marker property is cleared if the infowindow is closed.
                                               infowindow.addListener('closeclick', function() {
                                               infowindow.marker = null;
                                               });
                                             }
                                           },
+                                          //On error function to display the error message in the Infowindow.
                                           error: function(){
                                             infowindow.setContent('<div>Error Fetching Data</div>');
                                           }
                                       });
-                                    },
-                                    error: function (data) {
-                                       contentString = "Content Failed to Load Try Again!!!";
-                                       getinfowindow(contentString);
-                                    }
-                          });
+                                  },
+                        //On error function to display the error message in the Infowindow.
+                        error: function () {
+                                infowindow.setContent('<div>Error Fetching Data</div>');
+                                  }
+                        });
         }
+
+//Function to display the error message
+function erroralert(){
+  alert("Error loading data. Please check the connection or try again later.");
+}
+
 
 
